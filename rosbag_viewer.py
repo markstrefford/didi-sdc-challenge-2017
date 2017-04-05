0f87d04bb2ab1dc59208c64555a66caec678672f
Mark Strefford <mark@m20ventures.co.uk>
2017-04-05 14:27:04 +0100

    Added point cloud panorama
    Based on http://ronny.rest/blog/post_2017_04_03_point_cloud_panorama/

--- rosbag_viewer.py
+++ rosbag_viewer.py
@@ -8,6 +8,7 @@ import sensor_msgs.point_cloud2 as pc2
 from cv_bridge import CvBridge
 from mayavi import mlab
 import numpy as np
+from utils import point_cloud_to_panorama
 
 bag_file_dir = '/media/sf_vol/didi/Didi-Training-Release-1'
 image_size = (512, 1400, 3)
@@ -70,7 +71,7 @@ def load_rosbag_data(bag_file):
 
 # Main loop
 # TODO - Make sure it's main (if '__main__'... etc) and also add in argparse...
-lidar_scale = 10  # Initial guess!
+lidar_scale = 5  # Initial guess!
 ground_truth = -10 # Guess!! For z measurement, if below 0 then who cares??
 
 size = (640, 640)
@@ -79,39 +80,25 @@ center_y = size[1]/2
 center_z = 128
 color_b, color_g = 240, 240  # Change later?
 
-for image, lidar in load_rosbag_data('spin_shoreline.bag'):
-    # msplt.reset(x=lidar[:, 0], y=lidar[:, 1], z=lidar[:, 2],
-    #           scalars=lidar[:, 0] ** 2 + lidar[:, 1] ** 2)
-    # cv2.imshow('Image', image)
-    # cv2.waitKey(1)
-    #print ('LIDAR.shape: {}'.format(lidar.shape))
+for image, lidar in load_rosbag_data('approach_3.bag'):
     lidar_top = np.zeros((size[0],size[1], 3))
-    #lmin_x, lmax_x = min(lidar[:,0]), max(lidar[:,0])
-    #lmin_y, lmax_y = min(lidar[:,1]), max(lidar[:,1])
-    #lmin_z, lmax_z = min(lidar[:,2]), max(lidar[:,2])
-    #lmin_i, lmax_i = min(lidar[:,3]), max(lidar[:,3])
-    #print ('LIDAR.dims:')
-    #print ('  x: {}, {}'.format(lmin_x, lmax_x))
-    #print ('  y: {}, {}'.format(lmin_y, lmax_y))
-    #print ('  z: {}, {}'.format(lmin_z, lmax_z))
-    #print ('  i: {}, {}'.format(lmin_i, lmax_i))
-
     # Render the pointmap using np/cv2
     # See http://ronny.rest/tutorials/module/pointclouds_01/point_cloud_coordinates/
     for idx in range(lidar.shape[0]):
         _y = (-lidar[idx, 0] * lidar_scale) + center_y   # LIDAR +x is forward facing, CV2 +y is down from top left (0,0)
         _x = (-lidar[idx, 1] * lidar_scale) + center_x   # LIDAR +y is left facing, CV2 +x is right from top left (0,0)
         _z = lidar[idx, 2]
-        #_i = lidar[idx, 3]   # Intensity
-        #color = abs(int(_x ** 2 + _y ** 2))
-        #print ('_x: {}/{}, _y: {}/{}'.format(lidar[idx, 0],_x, lidar[idx, 1], _y))
+
         if (abs(_x) < size[0] and abs(_y) < size[1]) and _z > ground_truth:
             color_r = (_z + 10) * 10
             lidar_top[int(_y), int(_x)] = [color_b, color_g, color_r]
+
+    pano = point_cloud_to_panorama.point_cloud_to_panorama(lidar)
+
     cv2.imshow('LIDAR', lidar_top)
+    cv2.imshow('Panorama', pano)
     cv2.imshow('Camera', image)
     cv2.waitKey(1)
-    #print ('*' * 20)
 
 cv2.destroyAllWindows()
 
