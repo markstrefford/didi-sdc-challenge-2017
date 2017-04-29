64b173967ce9c928a4efe75cd83519bba491e6cb
Mark Strefford <mark@m20ventures.co.uk>
2017-04-29 21:23:51 +0100

    Initial training code written

---
+++ nn/data_reader.py
@@ -0,0 +1,120 @@
+"""
+data_reader.py
+
+Read training and validation data from file
+
+TODO: This will evolve over time to:
+ 1) handle more than 1 output folder
+ 2) get data as a ROS subscriber,
+ 3) etc.
+
+"""
+
+
+import nn
+import os
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from keras.models import Model, Sequential
+import glob
+
+# Get camera timestamp and index closest to the pointcloud timestamp
+#TODO Create a utility function
+def get_nearest_timestamp_and_index(data, timestamp):
+    index = data.ix[(data.timestamp - timestamp).abs().argsort()[:1]].index[0]
+    data_timestamp = data.ix[index].timestamp
+    return data_timestamp, index
+
+
+# Get info from tracklets
+#TODO Create a utility function
+def get_obstacle_from_tracklet(tracklet_file):
+    tracks = []
+    tracklets = parse_xml(tracklet_file)
+    for track in tracklets:
+        obj_size = track.size    #FIXME: Single obstacle per tracklet file
+        for translation, rotation, state, occlusion, truncation, amtOcclusion, amtBorders, absoluteFrameNumber in track:
+            tracks.append({
+                'frame': absoluteFrameNumber,
+                'translation': translation,
+                'rotation': rotation
+            })          # TODO: Add in other tracklets info as required
+    return obj_size, tracks
+
+
+# Use file-based training for now, single bag file
+class DataReader(object):
+    def __init__(self, base_dir):
+
+        self.training_dir = base_dir
+        self.lidar_top_dir = os.path.join(self.training_dir, '/processed/lidar_top')
+        self.load()
+
+    def load(self):
+        xs=[]
+        ys=[]
+
+        self.train_batch_pointer = 0
+        self.val_batch_pointer = 0
+        total = 0
+
+        #image_dir = os.path.join(self.training_dir, 'camera')
+        camera_csv = os.path.join(self.training_dir, 'capture_vehicle_camera.csv')   # We're driven by camera messages
+        pointcloud_csv = os.path.join(self.training_dir, 'capture_vehicle_pointcloud.csv')
+        object_csv = os.path.join(self.training_dir, 'objects_obs1_rear_rtk_interpolated.csv')
+        tracklet_file = os.path.join(self.training_dir, 'tracklet_labels.xml')
+
+        camera_data = pd.read_csv(camera_csv)  # ['timestamp']
+        obj_size, tracks = get_obstacle_from_tracklet(tracklet_file)
+
+        for file in sorted(glob.glob(self.lidar_top_dir + '/*.npy')):
+            lidar_file = os.path.join(self.lidar_top_dir, file)
+
+            #lidar = np.load(lidar_file)
+            xs.append(lidar_file)
+            timestamp = int(os.path.basename(file).replace('.npy', ''))
+            camera_timestamp, index = get_nearest_timestamp_and_index(camera_data, timestamp)
+            t = tracks[index].translation
+            r = tracks[index].rotation
+            y = np.array([t[0], t[1], t[2], r[0], r[1], r[2]])
+            ys.append(y)
+
+            total +=1
+
+        self.num_samples = len(xs)
+
+        self.train_xs = xs[:int(len(xs) * 0.8)]
+        self.train_ys = ys[:int(len(xs) * 0.8)]
+
+        self.val_xs = xs[-int(len(xs) * 0.2)]
+        self.val_ys = ys[-int(len(xs) * 0.2)]
+
+        self.num_train_samples = len(self.train_xs)
+        self.num_val_samples = len(self.val_xs)
+
+
+    def load_train_batch(self, batch_size):
+        x_out = []
+        y_out = []
+        for i in range(0, batch_size):
+            index = (self.train_batch_pointer + i) % self.num_train_samples
+            file = self.train_xs[index]
+            pointcloud = np.load(file)
+            x_out.append(pointcloud)
+            y_out.append(self.train_ys[index])
+        self.train_batch_pointer += batch_size
+        return x_out, y_out
+
+
+    def load_val_batch(self, batch_size):
+        x_out = []
+        y_out = []
+        for i in range(0, batch_size):
+            index = (self.val_batch_pointer + i) % self.num_val_samples
+            file = self.val_xs[index]
+            pointcloud = np.load(file)
+            x_out.append(pointcloud)
+            y_out.append(self.val_ys[index])
+        self.val_batch_pointer += batch_size
+        return x_out, y_out
--- nn/train.py
+++ nn/train.py
@@ -7,121 +7,92 @@ TODO: This will evolve over time to handle more than 1 bag, to become a ROS subs
 
 """
 
-
-import nn
 import os
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-from keras.models import Model, Sequential
-import glob
+import nn
+from data_reader import DataReader
 
-# Get camera timestamp and index closest to the pointcloud timestamp
-#TODO Create a utility function
-def get_nearest_timestamp_and_index(data, timestamp):
-    index = data.ix[(data.timestamp - timestamp).abs().argsort()[:1]].index[0]
-    data_timestamp = data.ix[index].timestamp
-    return data_timestamp, index
-
-
-# Get info from tracklets
-#TODO Create a utility function
-def get_obstacle_from_tracklet(tracklet_file):
-    tracks = []
-    tracklets = parse_xml(tracklet_file)
-    for track in tracklets:
-        obj_size = track.size    #FIXME: Single obstacle per tracklet file
-        for translation, rotation, state, occlusion, truncation, amtOcclusion, amtBorders, absoluteFrameNumber in track:
-            tracks.append({
-                'frame': absoluteFrameNumber,
-                'translation': translation,
-                'rotation': rotation
-            })          # TODO: Add in other tracklets info as required
-    return obj_size, tracks
+# TODO: Remove what's not needed here
+BATCH_SIZE = 32
+DATA_DIR = '/vol/didi/dataset2/tracklets/1pc/10pc'
+LOGDIR = './logs'
+CSV='data.csv'
+CHECKPOINT_EVERY = 100
+NUM_STEPS = int(1e4)
+CKPT_FILE = 'model.ckpt'
+LEARNING_RATE = 1e-3
+KEEP_PROB = 0.8
+L2_REG = 0
+EPSILON = 0.001
+MOMENTUM = 0.9
 
 
-# Use file-based training for now, single bag file
-class data_reader(object):
-    def __init__(self, batch_size, start, base_dir):
-        self.batch_size = batch_size
-        self.start = start
-        self.training_dir = base_dir
-        self.lidar_top_dir = os.path.join(self.training_dir, '/processed/lidar_top')
-        self.load()
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Udacity Challenge Training')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
+                        action='store', dest='batch_size', help='Number of [camera] samples in batch.')
+    parser.add_argument('--data_dir', '--data', type=str, default=DATA_DIR,
+                        action='store', dest='data_dir', help='The directory containing the training data.')
+    # parser.add_argument('--data_csv', '--csv', type=str, default=CSV,
+    #                     action='store', dest='csv', help='The csv containing the training data.')
+    # parser.add_argument('--logdir', type=str, default=LOGDIR,
+    #                     help='Directory for log files.')
+    parser.add_argument('--restore_from', type=str, default=None,
+                        help='Checkpoint file to restore model weights from.')
+    parser.add_argument('--checkpoint_every', type=int, default=CHECKPOINT_EVERY,
+                        help='How many steps to save each checkpoint after')
+    parser.add_argument('--num_steps', type=int, default=NUM_STEPS,
+                        help='Number of training steps.')
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE,
+                        help='Learning rate for training.')
+    parser.add_argument('--keep_prob', type=float, default=KEEP_PROB,
+                        help='Dropout keep probability.')
+    parser.add_argument('--l2_reg', type=float,
+                        default=L2_REG)
+    return parser.parse_args()
 
-    def load(self):
-        xs=[]
-        ys=[]
+def main():
+    args=get_arguments()
 
-        self.train_batch_pointer = 0
-        self.val_batch_pointer = 0
-        total = 0
+    start_step = 0
+    LossHistory, model = nn.top_nn()
+    summary = model.summary()
+    print (summary)     # TODO: Write to disk together with diagram (see keras.model_to_dot)
 
-        #image_dir = os.path.join(self.training_dir, 'camera')
-        camera_csv = os.path.join(self.training_dir, 'capture_vehicle_camera.csv')   # We're driven by camera messages
-        pointcloud_csv = os.path.join(self.training_dir, 'capture_vehicle_pointcloud.csv')
-        object_csv = os.path.join(self.training_dir, 'objects_obs1_rear_rtk_interpolated.csv')
-        tracklet_file = os.path.join(self.training_dir, 'tracklet_labels.xml')
+    data_reader = DataReader(args.data_dir, args.csv)
 
-        camera_data = pd.read_csv(camera_csv)  # ['timestamp']
-        obj_size, tracks = get_obstacle_from_tracklet(tracklet_file)
+    #FIXME: Based on number of steps, convert to number of epochs
+    for i in range(start_step, start_step + args.num_steps):
+        xs, ys = data_reader.load_train_batch(batch_size=args.batch_size)
+        train_error = model.train_on_batch(xs, ys)
+        print ('{}/{}: Training loss: {}'.format(i, start_step + args.num_steps, train_error))
 
-        for file in sorted(glob.glob(self.lidar_top_dir + '/*.npy')):
-            lidar_file = os.path.join(self.lidar_top_dir, file)
+        if i % 10 == 0:
+            xs, ys = data_reader.load_val_batch(batch_size=args.batch_size)
+            val_error = model.test_on_batch(xs, ys)
+            print ('{}/{}: Validation loss: {}'.format(i, start_step + args.num_steps, val_error))
 
-            #lidar = np.load(lidar_file)
-            xs.append(lidar_file)
-            timestamp = int(os.path.basename(file).replace('.npy', ''))
-            camera_timestamp, index = get_nearest_timestamp_and_index(camera_data, timestamp)
-            t = tracks[index].translation
-            r = tracks[index].rotation
-            y = np.array([t[0], t[1], t[2], r[0], r[1], r[2]])
-            ys.append(y)
+            if i > 0 and i % args.checkpoint_every == 0:
+                if not os.path.exists(args.logdir):
+                    os.makedirs(args.logdir)
+                filename = 'model-step-%d-val-%g.ckpt' % (i, val_error)
+                checkpoint_path = os.path.join(args.logdir, filename)
+                model.save(checkpoint_path)
+                print('Model saved in file: {}'.format(filename))
 
-            total +=1
+            # TODO - Add in exit if vol_loss < min_loss
 
-        self.num_samples = len(xs)
+            # TODO - Write logs (if we can in Keras!)
 
-        self.train_xs = xs[:int(len(xs) * 0.8)]
-        self.train_ys = ys[:int(len(xs) * 0.8)]
+            # TODO - Write loss history and other metrics for graphing...
 
-        self.val_xs = xs[-int(len(xs) * 0.2)]
-        self.val_ys = ys[-int(len(xs) * 0.2)]
+    # Training has finished
+    filename = 'model-final-step-%d-val-%g.ckpt' % (i, val_error)
+    checkpoint_path = os.path.join(args.logdir, filename)
+    model.save_weights(checkpoint_path)
+    print ('Final model saved as {}', filename)
 
-        self.num_train_samples = len(self.train_xs)
-        self.num_val_samples = len(self.val_xs)
-
-
-    def load_train_batch(self, batch_size):
-        x_out = []
-        y_out = []
-        for i in range(0, batch_size):
-            index = (self.train_batch_pointer + i) % self.num_train_samples
-            file = self.train_xs[index]
-            pointcloud = np.load(file)
-            x_out.append(pointcloud)
-            y_out.append(self.train_ys[index])
-        self.train_batch_pointer += batch_size
-        return x_out, y_out
-
-
-    def load_val_batch(self, batch_size):
-        x_out = []
-        y_out = []
-        for i in range(0, batch_size):
-            index = (self.val_batch_pointer + i) % self.num_val_samples
-            file = self.val_xs[index]
-            pointcloud = np.load(file)
-            x_out.append(pointcloud)
-            y_out.append(self.val_ys[index])
-        self.val_batch_pointer += batch_size
-        return x_out, y_out
-
-
-
-
-
-
+if __name__ = '__main__':
+    main()
 
 
 
