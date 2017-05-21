690e4b1675d5de39f3eef892fafbbd526bde29f6
Mark Strefford <mark@m20ventures.co.uk>
2017-05-21 11:09:14 +0100

    Debug

--- pointcloud_utils/create_pointcloud_images.py
+++ pointcloud_utils/create_pointcloud_images.py
@@ -44,6 +44,7 @@ def get_camera_timestamp_and_index(camera_data, pointcloud_timestamp, timestamp_
     camera_index = camera_data.ix[(camera_data.timestamp - pointcloud_timestamp).abs().argsort()[:1]].index[0]
 
     proposed_index = camera_index + timestamp_offset
+    print ('Proposed index: {} = camera_index: {} + timestamp_offset: {}'.format(proposed_index, camera_index, timestamp_offset))
     if timestamp_offset > 0:
         actual_index = min(proposed_index,len(camera_data))
     elif timestamp_offset < 0:
@@ -51,7 +52,7 @@ def get_camera_timestamp_and_index(camera_data, pointcloud_timestamp, timestamp_
     else:
         actual_index = proposed_index
 
-    print ('Proposed index: {}, actual index: {}'.format(proposed_index, actual_index))
+    print ('Actual index: {}'.format(actual_index))
     #camera_timestamp = camera_data.ix[camera_index].timestamp
     camera_timestamp = camera_data.ix[actual_index].timestamp
     return camera_timestamp, camera_index
