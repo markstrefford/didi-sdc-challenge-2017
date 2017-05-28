4a16f431c4bd0ba4ba53129fd82cca205330612a
Mark Strefford <mark@m20ventures.co.uk>
2017-05-28 18:54:15 +0100

    Determining object centroid
    Also adding in trainable flag for cnn in case we use regressor to
    determine coordinates

--- nn/nn.py
+++ nn/nn.py
@@ -142,7 +142,7 @@ def camera_nn(model, num_classes, weights_path=None, w_regularizer = None, b_reg
     return model
 
 
-def top_nn(weights_path=None, b_regularizer = None, w_regularizer=None):
+def top_nn(weights_path=None, b_regularizer = None, w_regularizer=None, cnn_trainable=True):
     class LossHistory(Callback):
         def on_train_begin(self, logs={}):
             self.losses = []
@@ -151,42 +151,42 @@ def top_nn(weights_path=None, b_regularizer = None, w_regularizer=None):
             self.losses.append(logs.get('loss'))
 
     inputs = Input((top_x, top_y, top_z))
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
-    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
-    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv5)
 
-    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
-    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
-    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', trainable=cnn_trainable)(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv6)
 
-    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
-    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', trainable=cnn_trainable)(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv7)
 
-    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
-    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
-    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', trainable=cnn_trainable)(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv8)
 
-    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
-    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
-    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same', trainable=cnn_trainable)(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same', trainable=cnn_trainable)(conv9)
 
-    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid', trainable=cnn_trainable)(conv9)
 
     model = Model(inputs=[inputs], outputs=[conv10])
     model.compile(optimizer=Adam(lr=1e-4),
